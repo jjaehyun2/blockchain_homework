9d9d3498f99422d0a4e9d14e2efd7ca9ff3c7fdc
ToBecomputerHuman <brian951862@naver.com>
2025-10-08 22:45:37 +0900

    added transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -7,14 +7,14 @@ contract MyToken {
     uint8 public decimals; //uint8 --> 8 bit unsigned int ,...., uint256
     //public : 자동으로 getter 생성
     uint256 public totalSupply; //전체 몇개 발행
-    mapping(address => uint256) public balanceOf;//누가 몇개
+    mapping(address => uint256) public balanceOf;//누가 몇개//조회하는거는 동일한 값을 리턴해준다 tx를 만들 필요없음
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _amount) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
          //msg.sender : 배포한 사람
-        _mint(1 * 10**uint256(decimals), msg.sender); //1 * 10^18
+        _mint(_amount * 10 ** uint256(decimals), msg.sender);
     
     }
     //화페 발행시 mint
@@ -25,4 +25,9 @@ contract MyToken {
         balanceOf[owner] += amount;
     }
 
+    function transfer(uint256 amount, address to) external { //tx로 호출됨 -> tx생성
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+    }
 }
