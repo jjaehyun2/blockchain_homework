eb51d22136bd98d0a3d40f4d9c9f5913b201231c
ToBecomputerHuman <brian951862@naver.com>
2025-10-27 19:25:58 +0900

    add reward test

--- test/TinyBank.ts
+++ test/TinyBank.ts
@@ -48,4 +48,24 @@ describe("TinyBank", () => {
             expect(await TinyBankC.staked(signer0.address)).to.equal(0n);
         });
     });
+
+    describe("reward", async () => {
+        it("should reward 1MT every blocks", async () => {
+            const signer0 = signers[0];
+            const stakingAmount = hre.ethers.parseUnits("50", DECIMALS);
+            await myTokenC.approve(await TinyBankC.getAddress(), stakingAmount);
+            await TinyBankC.stake(stakingAmount);
+
+            const BLOCKS = 5n;
+            const transferAmount = hre.ethers.parseUnits("1", DECIMALS);
+            for (let i = 0; i < BLOCKS; i++) {
+                await myTokenC.transfer(transferAmount, signer0.address);
+            }
+
+            await TinyBankC.withdraw(stakingAmount);
+            expect(await myTokenC.balanceOf(signer0.address)).to.equal(
+                hre.ethers.parseUnits((BLOCKS + MINTING_AMOUNT + 1n).toString())
+            );
+        });
+    });
 });
