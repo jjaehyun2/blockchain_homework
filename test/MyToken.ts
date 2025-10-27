fe27f52bfbe0963cd2d5545eb967ba04cfd10b37
ToBecomputerHuman <brian951862@naver.com>
2025-10-27 20:03:19 +0900

    add malicious minting try test

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -31,7 +31,14 @@ describe("My Token", () => {
             const signer0 = signers[0];
             expect(await myTokenC.balanceOf(signer0.address)).to.equal(MINTING_AMOUNT * 10n**DECIMALS);
         });
-    })    
+        
+        //TDD : Test Driven Development
+        it("should return or revert when minting infinitly", async () => {
+            const mintingAgainAmount = hre.ethers.parseUnits("10000", DECIMALS);
+            const hacker = signers[2];
+            await expect(myTokenC.connect(hacker).mint(mintingAgainAmount, hacker.address)).to.be.revertedWith("You are not authorized to manage this token");
+        });
+    });
     //1MTK = 10^18
 
     describe("Transfer", () => {
