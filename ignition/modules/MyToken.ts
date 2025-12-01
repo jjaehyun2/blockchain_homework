33a266b3bbeba7b50eca5d05a2849b2972ad6e82
jjaehyun2 <128404@naver.com>
2025-12-02 06:15:28 +0900

    last task

--- ignition/modules/MyToken.ts
+++
@@ -1,8 +0,0 @@
-import {buildModule} from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("MyTokenDeploy", (m) => {
-    const myToken = m.contract("MyToken", ["My Token", "MTK", 18]);
-
-    return {myToken}; //감싸서
-    
-})
--- test/constant.ts
+++
@@ -1,2 +0,0 @@
-export const MINTING_AMOUNT = 100n;
-export const DECIMALS = 18n;
