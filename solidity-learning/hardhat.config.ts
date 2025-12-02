a355ad24b63f29e0ee65da41165c4400b70787ce
jjaehyun2 <81456273+jjaehyun2@users.noreply.github.com>
2025-12-02 13:35:04 +0900

    rewrite few codes

--- solidity-learning/hardhat.config.ts
+++ solidity-learning/hardhat.config.ts
@@ -30,20 +30,5 @@ const config: HardhatUserConfig = {
       },
     ],
   }, 
-  etherscan: {
-    apiKey: {
-      kairos: "unnecessary",
-    },
-    customChains: [
-      {
-        network: "kairos",
-        chainId: 1001,
-        urls: {
-          apiURL: "https://kairos-api.kaiascan.io/hardhat-verify",
-          browserURL: "https://kairos.kaiascan.io",
-        },
-      },
-    ],
-  },
 };
 export default config;
